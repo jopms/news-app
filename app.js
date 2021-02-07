ea31df4df81983ea213d4b0cd67305f2f3a409b2
jopms <joao.34@hotmail.com>
2021-02-07 21:26:55 +0000

    Added new transitions to the created divs and also now showing the new's information

--- app.js
+++ app.js
@@ -9,17 +9,46 @@ function getNews (url){
         .then ((data) => updateWindow(data))
 }
 
-function updateWindow(data){
-    
-    //(document.getElementsByClassName("header"))[0].className === "header" ? (document.getElementsByClassName("header"))[0].className = "headerUpdated" : null;
+function updateWindow(news){
+
     ((document.getElementsByClassName("header"))[0]).style.transform = "translateY(0%)";
     ((document.getElementsByClassName("header"))[0]).style.paddingTop = "1rem";
     ((document.getElementsByClassName("header"))[0]).style.boxShadow = "0px 4px 5px -4px rgba(0, 0, 0, 0.3)";
+
+    document.getElementById("news").innerHTML=`
+        <div class ="news fade">
+        <h2 class="title2">${news.data[0].title}</h2>
+        <p><h3 class="description">${news.data[0].description}</h3></p>
+        <p><h4 class="source">${news.data[0].source}<h4></p>
+        </div>
+
+        <div class ="news fade">
+        <h2 class="title2">${news.data[1].title}</h2>
+        <p><h3 class="description">${news.data[1].description}</h3></p>
+        <p><h4 class="source">${news.data[1].source}<h4></p>
+        </div>
+
+        <div class ="news fade">
+        <h2 class="title2">${news.data[2].title}</h2>
+        <p><h3 class="description">${news.data[2].description}</h3></p>
+        <p><h4 class="source">${news.data[2].source}<h4></p>
+        </div>
+    `;
+    requestAnimationFrame(() => {
+    document.getElementsByClassName("news")[0].classList.remove("fade");
+     document.getElementsByClassName("news")[1].classList.remove("fade");
+     document.getElementsByClassName("news")[2].classList.remove("fade");
+    });
+ 
+
+
+    
+    
 }
 
 function updateInfo(info){
-    //getNews(url+`&keywords=${info}`);
-    updateWindow(info);
+    getNews(url+`&keywords=${info}`);
+    
 
 }
 
