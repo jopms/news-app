3c89235904777e1f6181af73f784b248505c9424
jopms <joao.34@hotmail.com>
2021-02-09 13:14:04 +0000

    Category now changes the url, html updated 25 values of news everytime anything is searched

--- app.js
+++ app.js
@@ -1,69 +1,58 @@
-const api = "3752898342c7441f7444634b55547d12";
-const url = `http://api.mediastack.com/v1/news?access_key=${api}&languages=pt`;
+const newsData = {
+    api: "3752898342c7441f7444634b55547d12",
+    category: "general",
+    url() { return `http://api.mediastack.com/v1/news?access_key=${this.api}&languages=pt&country=pt&sort=published_desc&categories=${this.category}` }
+}
 
+const updateCategory = (cat) => {
+    newsData.category = cat;
+}
 
-
-function getNews (url){ 
+const getNews = (url) => {
+    console.log(url);
     fetch(url)
-        .then ((news) => news.json())
-        .then ((data) => updateWindow(data))
+        .then((news) => news.json())
+        .then((data) => updateWindow(data))
 }
 
-function updateWindow(news){
+const updateWindow = (news) => {
+    const headerStyle = ((document.getElementsByClassName("header"))[0]).style;
 
-    ((document.getElementsByClassName("header"))[0]).style.transform = "translateY(0%)";
-    ((document.getElementsByClassName("header"))[0]).style.paddingTop = "1rem";
-    ((document.getElementsByClassName("header"))[0]).style.boxShadow = "0px 4px 5px -4px rgba(0, 0, 0, 0.3)";
+    headerStyle.transform = "translateY(0%)";
+    headerStyle.paddingTop = "1rem";
+    headerStyle.boxShadow = "0px 4px 5px -4px rgba(0, 0, 0, 0.3)";
 
-    document.getElementById("news").innerHTML=`
-        <div class ="news fade">
-        <h2 class="title2">${news.data[0].title}</h2>
-        <p><h3 class="description">${news.data[0].description}</h3></p>
-        <p><h4 class="source">${news.data[0].source}<h4></p>
-        </div>
+    const organizeData = (news.data).map((n, i) => {
+        return (
+            `   <div class ="news fade">
+                <a href="${news.data[i].url}"><h2 class="title2">${news.data[i].title}</h2></a>
+                <p><h3 class="description">${news.data[i].description}</h3></p>
+                <p><h4 class="source">${news.data[i].source} - ${(news.data[i].published_at).slice(0, 10)}<h4></p>
+                </div>`
+        )
+    }).join("");
 
-        <div class ="news fade">
-        <h2 class="title2">${news.data[1].title}</h2>
-        <p><h3 class="description">${news.data[1].description}</h3></p>
-        <p><h4 class="source">${news.data[1].source}<h4></p>
-        </div>
+    document.getElementById("news").innerHTML = organizeData;
 
-        <div class ="news fade">
-        <h2 class="title2">${news.data[2].title}</h2>
-        <p><h3 class="description">${news.data[2].description}</h3></p>
-        <p><h4 class="source">${news.data[2].source}<h4></p>
-        </div>
-    `;
     requestAnimationFrame(() => {
-    document.getElementsByClassName("news")[0].classList.remove("fade");
-     document.getElementsByClassName("news")[1].classList.remove("fade");
-     document.getElementsByClassName("news")[2].classList.remove("fade");
+        news.data.forEach((n, i) => {
+            document.getElementsByClassName("news")[i].classList.remove("fade");
+        });
     });
- 
-
-
-    
-    
 }
 
-function updateInfo(info){
-    getNews(url+`&keywords=${info}`);
-    
-
+const updateInfo = (info) => {
+    getNews(newsData.url() + `&keywords=${info}`);
 }
 
-function searchValue (e){
-    if(e.key === "Enter"){
+const searchValue = (e) => {
+    if (e.key === "Enter") {
         updateInfo(this.value);
         this.value = "";
-    }else return null
-
-    
+    } else return null
 }
 
-
-
-function clearInput(){
+const clearInput = () => {
     document.getElementById("search").value = "";
 }
 
